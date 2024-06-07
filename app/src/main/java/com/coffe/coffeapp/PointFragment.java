746ec675547c7a775a2bcc08b7e68fd799c0ce64
Yudhistira <yudhistirawsb@gmail.com>
2024-06-07 20:07:13 +0700

    Cek Database

--- app/src/main/java/com/coffe/coffeapp/PointFragment.java
+++ app/src/main/java/com/coffe/coffeapp/PointFragment.java
@@ -3,36 +3,32 @@ package com.coffe.coffeapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.coffe.coffeapp.model.DataProducts;
-
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link PointFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class PointFragment extends Fragment {
-    ArrayList<PointModel> list;
-    RecyclerView recyclerView;
-    GridLayoutManager gridLayoutManager;
+    public static PointFragment newInstance(String param1, String param2) {
+        PointFragment fragment = new PointFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        gridLayoutManager = new GridLayoutManager(getActivity(), 2, gridLayoutManager.VERTICAL,false){
-            @Override
-            public boolean canScrollVertically() {
-                return false;
-            }
-        };
-        recyclerView.setLayoutManager(gridLayoutManager);
-        PointAdapter pointAdapter = new PointAdapter(list);
-        recyclerView.setAdapter(pointAdapter);
-        pointAdapter.notifyDataSetChanged();
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_point, container, false);
     }
