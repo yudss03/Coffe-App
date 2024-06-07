3c39b6a107bcac349db8f4deabd08eca9e349258
Farrel <farrellutfi150@gmail.com>
2024-06-07 19:05:47 +0700

    PointAdapter

--- app/src/main/java/com/coffe/coffeapp/PointFragment.java
+++ app/src/main/java/com/coffe/coffeapp/PointFragment.java
@@ -11,8 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.coffe.coffeapp.model.DataProducts;
-
 import java.util.ArrayList;
 import java.util.List;
 
